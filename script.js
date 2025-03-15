9ebf6e4540e5085aab77d4a0469d2d457611d95a
fredieposh <maor1234@gmail.com>
2025-03-15 16:21:16 +0200

    Add function "playRound"
    Needed function to manage a round of Rock Paper Scissors.

--- script.js
+++ script.js
@@ -9,4 +9,53 @@ function getComputerChoice() {
     };
 }
 
-console.log(getComputerChoice());
+function getUserChoice() {
+    return prompt("Enter Your Move");
+}
+
+function playRound(humanChoice, computerchoice) {
+    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
+    computerchoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
+
+    if (humanChoice === "Paper") {
+        if (computerchoice === "Paper") {
+            console.log(`That's a tie. You both chose ${humanChoice}`);
+        } else if (computerchoice === "Rock") {
+            console.log(`You win! ${humanChoice} beats ${computerchoice}`);
+            humanScore ++ ;
+            return ;
+        } else {
+            console.log(`You lose! ${computerchoice} beats ${humanChoice}`);
+            computerScore ++;
+            return ;
+        }
+    } else if ((humanChoice === "Rock")) {
+        if (computerchoice === "Paper") {
+            console.log(`You lose! ${computerchoice} beats ${humanChoice}`);
+            computerScore ++;
+            return ;
+        } else if (computerchoice === "Rock") {
+            console.log(`That's a tie. You both chose ${humanChoice}`);
+        } else {
+            console.log(`You win! ${humanChoice} beats ${computerchoice}`);
+            humanScore ++ ;
+            return ;
+        }
+    } else {
+        if (computerchoice === "Paper") {
+            console.log(`You win! ${humanChoice} beats ${computerchoice}`);
+            humanScore ++ ;
+            return ;
+        } else if (computerchoice === "Rock") {
+            console.log(`You lose! ${computerchoice} beats ${humanChoice}`);
+            computerScore ++;
+            return ;
+        } else {
+            console.log(`That's a tie. You both chose ${humanChoice}`);
+        }        
+    }
+}
+
+let humanScore = 0,
+    computerScore = 0;
+
