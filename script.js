368a1622150bdd920968ec405b728a506b88f719
fredieposh <maor1234@gmail.com>
2025-03-15 16:44:18 +0200

    Add "playGame" function
    Need this funciton in order to play multiple number of rounds.

--- script.js
+++ script.js
@@ -15,47 +15,62 @@ function getUserChoice() {
 
 function playRound(humanChoice, computerchoice) {
     humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
-    computerchoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
+    computerchoice = computerchoice.charAt(0).toUpperCase() + computerchoice.slice(1).toLowerCase();
 
     if (humanChoice === "Paper") {
         if (computerchoice === "Paper") {
             console.log(`That's a tie. You both chose ${humanChoice}`);
+            return;
         } else if (computerchoice === "Rock") {
             console.log(`You win! ${humanChoice} beats ${computerchoice}`);
-            humanScore ++ ;
-            return ;
+            humanScore++;
+            return;
         } else {
             console.log(`You lose! ${computerchoice} beats ${humanChoice}`);
-            computerScore ++;
-            return ;
+            computerScore++;
+            return;
         }
     } else if ((humanChoice === "Rock")) {
         if (computerchoice === "Paper") {
             console.log(`You lose! ${computerchoice} beats ${humanChoice}`);
-            computerScore ++;
-            return ;
+            computerScore++;
+            return;
         } else if (computerchoice === "Rock") {
             console.log(`That's a tie. You both chose ${humanChoice}`);
+            return;
         } else {
             console.log(`You win! ${humanChoice} beats ${computerchoice}`);
-            humanScore ++ ;
-            return ;
+            humanScore++;
+            return;
         }
     } else {
         if (computerchoice === "Paper") {
             console.log(`You win! ${humanChoice} beats ${computerchoice}`);
-            humanScore ++ ;
-            return ;
+            humanScore++;
+            return;
         } else if (computerchoice === "Rock") {
             console.log(`You lose! ${computerchoice} beats ${humanChoice}`);
-            computerScore ++;
-            return ;
+            computerScore++;
+            return;
         } else {
             console.log(`That's a tie. You both chose ${humanChoice}`);
-        }        
+            return;
+        }
     }
+}
+
+function playGame() {
+    const humanSelection = getUserChoice();
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
+    console.log(`human: ${humanScore}\ncomputer: ${computerScore}`);
 }
 
 let humanScore = 0,
     computerScore = 0;
 
+for (let i = 0; i < 5; i++) {
+    console.log(`Round ${i + 1}:`)
+    playGame();
+}
