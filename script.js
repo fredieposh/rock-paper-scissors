4149eeb757b887f443e81e2395430e27e932b8d9
fredieposh <maor1234@gmail.com>
2025-03-23 22:34:53 +0200

    Change functions and html page
    For enabling the score, the results of each round, and the game
    result;

--- script.js
+++ script.js
@@ -9,47 +9,70 @@ function getComputerChoice() {
     };
 }
 
+function displayRoundResults(humanChoice, computerchoice) {
+    document.querySelector("#userResult").textContent = `You chose ${humanChoice}`;
+    document.querySelector("#computerResult").textContent = `The computer chose ${computerchoice}`;
+}
+
 function playRound(humanChoice, computerchoice) {
     humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
     computerchoice = computerchoice.charAt(0).toUpperCase() + computerchoice.slice(1).toLowerCase();
 
     if (humanChoice === "Paper") {
         if (computerchoice === "Paper") {
-            console.log(`That's a tie. You both chose ${humanChoice}`);
+            displayRoundResults(humanChoice, computerchoice);
+            document.querySelector("#resultComment").textContent = `That's a tie. You both chose ${humanChoice}`;
+            // console.log(`That's a tie. You both chose ${humanChoice}`);
             return;
         } else if (computerchoice === "Rock") {
-            console.log(`You win! ${humanChoice} beats ${computerchoice}`);
+            displayRoundResults(humanChoice, computerchoice);
+            document.querySelector("#resultComment").textContent = `You win! ${humanChoice} beats ${computerchoice}`;
+            // console.log(`You win! ${humanChoice} beats ${computerchoice}`);
             humanScore++;
             return;
         } else {
-            console.log(`You lose! ${computerchoice} beats ${humanChoice}`);
+            displayRoundResults(humanChoice, computerchoice);
+            document.querySelector("#resultComment").textContent = `You lose! ${computerchoice} beats ${humanChoice}`;
+            // console.log(`You lose! ${computerchoice} beats ${humanChoice}`);
             computerScore++;
             return;
         }
     } else if ((humanChoice === "Rock")) {
         if (computerchoice === "Paper") {
-            console.log(`You lose! ${computerchoice} beats ${humanChoice}`);
+            displayRoundResults(humanChoice, computerchoice);
+            document.querySelector("#resultComment").textContent = `You lose! ${computerchoice} beats ${humanChoice}`;
+            // console.log(`You lose! ${computerchoice} beats ${humanChoice}`);
             computerScore++;
             return;
         } else if (computerchoice === "Rock") {
-            console.log(`That's a tie. You both chose ${humanChoice}`);
+            displayRoundResults(humanChoice, computerchoice);
+            document.querySelector("#resultComment").textContent = `That's a tie. You both chose ${humanChoice}`;
+            // console.log(`That's a tie. You both chose ${humanChoice}`);
             return;
         } else {
-            console.log(`You win! ${humanChoice} beats ${computerchoice}`);
+            displayRoundResults(humanChoice, computerchoice);
+            document.querySelector("#resultComment").textContent = `You win! ${humanChoice} beats ${computerchoice}`;
+            // console.log(`You win! ${humanChoice} beats ${computerchoice}`);
             humanScore++;
             return;
         }
     } else {
         if (computerchoice === "Paper") {
-            console.log(`You win! ${humanChoice} beats ${computerchoice}`);
+            displayRoundResults(humanChoice, computerchoice);
+            document.querySelector("#resultComment").textContent = `You win! ${humanChoice} beats ${computerchoice}`;
+            // console.log(`You win! ${humanChoice} beats ${computerchoice}`);
             humanScore++;
             return;
         } else if (computerchoice === "Rock") {
-            console.log(`You lose! ${computerchoice} beats ${humanChoice}`);
+            displayRoundResults(humanChoice, computerchoice);
+            document.querySelector("#resultComment").textContent = `You lose! ${computerchoice} beats ${humanChoice}`;
+            // console.log(`You lose! ${computerchoice} beats ${humanChoice}`);
             computerScore++;
             return;
         } else {
-            console.log(`That's a tie. You both chose ${humanChoice}`);
+            displayRoundResults(humanChoice, computerchoice);
+            document.querySelector("#resultComment").textContent = `That's a tie. You both chose ${humanChoice}`;
+            // console.log(`That's a tie. You both chose ${humanChoice}`);
             return;
         }
     }
@@ -65,12 +88,22 @@ function playGame(userChoice) {
 
 let humanScore = 0,
     computerScore = 0;
+const humanScoreDisplay = document.querySelector("#UserScore");
+const computerScoreDisplay = document.querySelector("#ComputerScore");
 
-const choiceButtonsDiv = document.querySelector("div");
+humanScoreDisplay.textContent = humanScore;
+computerScoreDisplay.textContent = computerScore;
+
+const choiceButtonsDiv = document.querySelector("#choiceButtons");
 
 choiceButtonsDiv.addEventListener("click", (e) => {
-    if (humanScore < 5 && computerScore < 5)
-    playGame(e.target.textContent);
+    if (humanScore < 5 && computerScore < 5) {
+        playGame(e.target.textContent);
+        humanScoreDisplay.textContent = humanScore;
+        computerScoreDisplay.textContent = computerScore;
+    }
+    if(humanScore === 5) document.querySelector("#winner").textContent = "You are the winner!\nRefresh the page to start again.";
+    if(computerScore === 5) document.querySelector("#winner").textContent = "The computer is the winner!\nRefresh the page to start again.";
 });  
 
   
